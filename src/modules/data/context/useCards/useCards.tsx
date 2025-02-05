88ec36607703a14fde2f805140714e24e9df5886
Napoleon Izquieta <napoleon.izquieta@promocionesfarma.com>
2025-02-05 18:43:12 +0100

    ✨ Adding useCards context

---
+++ src/modules/data/context/useCards/useCards.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import { useData } from "../useData/useData";
+import { Card } from "../../types/Card";
+
+interface CardsContextType {
+  cardsList: Card[];
+  addCard: (id: string) => string | void;
+  removeCard: (id: string) => string | void;
+  isLoading: boolean;
+}
+
+const CardsContext = createContext<CardsContextType | undefined>(undefined);
+
+export const CardsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [storedCards, setStoredCards] = useState<Card[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { cards } = useData();
+
+  useEffect(() => {
+    getCardsList();
+  }, []);
+
+  const getCardsList = async (): Promise<never[] | undefined> => {
+    try {
+      const storedValue = await getCookie("cards-list");
+
+      if (!storedValue) {
+        return [];
+      }
+
+      const listOfCards: string[] = JSON.parse(storedValue);
+
+      const populatedStoredCards = cards.filter((card) => listOfCards.includes(card.id));
+
+      setStoredCards(populatedStoredCards);
+      setIsLoading(false);
+    } catch (error: any) {
+      console.error("Error parsing cards-list cookie: ", error.message);
+      setStoredCards([]);
+    }
+  };
+
+  const addCard = (id: string): string | void => {
+    const hasStoredCard = storedCards.find((card) => card.id === id);
+
+    if (hasStoredCard) {
+      return "Card already added";
+    }
+
+    const newStoredCard = cards.find((card) => card.id === id);
+
+    if (!newStoredCard) {
+      return "Card not found";
+    }
+
+    const newStoredCardList = [...storedCards, newStoredCard];
+    updateCardsList(newStoredCardList);
+
+    return "Card added to your list";
+  };
+
+  const removeCard = (id: string): string | void => {
+    const hasStoredCard = storedCards.find((card) => card.id === id);
+
+    if (!hasStoredCard) {
+      return "Card id not found";
+    }
+
+    const newStoredCardList = storedCards.filter((card) => card.id !== id);
+    updateCardsList(newStoredCardList);
+
+    return "Card removed from your list";
+  };
+
+  const updateCardsList = (cardsList: Card[]): void => {
+    setStoredCards(cardsList);
+
+    const storedCardsIds = cardsList.map((card) => card.id);
+    setCookie("cards-list", JSON.stringify(storedCardsIds), { path: "/" });
+  };
+
+  const value: CardsContextType = React.useMemo(
+    () => ({ cardsList: storedCards, addCard, removeCard, isLoading }),
+    [storedCards, addCard, removeCard]
+  );
+  return <CardsContext.Provider value={value}>{children}</CardsContext.Provider>;
+};
+
+export const useCards = (): CardsContextType => {
+  const context = useContext(CardsContext);
+  if (!context) {
+    throw new Error("useCards must be used within a CardsProvider");
+  }
+  return context;
+};
